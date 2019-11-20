d138d6947b359747a2043641ab3c5a646654becf
TiavinaMichael <ralainirinatiavina@gmail.com>
2019-11-20 11:23:27 +0100

    refactoring

--- src/Circle.js
+++ src/Circle.js
@@ -6,7 +6,7 @@ const CheckMark = props => {
     <svg height="400" width="400">
       <circle cx="60" cy="150" r="50" ry="50" fill="none" stroke="black" />
       <path
-        d="M10,45 A 50 20 0 1 1 110,50"
+        d="M10,50 A 50 20 0 1 1 110,50"
         fill="none"
         stroke="red"
         stroke-width="4"
@@ -22,7 +22,7 @@ const CheckMark = props => {
 
       <circle cx="60" cy="150" r="50" fill="none" stroke="black" />
       <path
-        d="M10,145 A 50 50 0 1 1 110,150"
+        d="M10,150 A 50 50 0 1 1 110,150"
         fill="none"
         stroke="red"
         stroke-width="24"
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react"
 import { render } from "react-dom"
-import CheckMark from './CheckMark'
+import Circle from './Circle'
+
+const App = () => {
+  return (
+    <Circle fill="red" width={300} height={300}/>
+  )
+}
 
 render(
-  <CheckMark fill="red" width={300} height={300}/>,
+  <App />,
   document.getElementById("root")
 )
