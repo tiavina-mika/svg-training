8443d363acb6ce822631d4f5f8d0beee7815668b
TiavinaMichael <ralainirinatiavina@gmail.com>
2019-11-20 12:04:34 +0100

    opacity

---
+++ src/Opacity.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles(theme => ({
+  circle1: {
+    fill: 'magenta',
+    stroke: 'black',
+    strokeWidth: 1,
+    fillOpacity: 0.6,
+  },
+  circle2: {
+    fill: 'cyan',
+    stroke: 'black',
+    strokeWidth: 1,
+    fillOpacity: 0.5,
+  },
+  circle3: {
+    fill: 'yellow',
+    stroke: 'black',
+    strokeWidth: 1,
+    fillOpacity: 0.3,
+  },
+  
+}))
+
+const Visage = props => {
+  const { width, height } = props;
+  const classes = useStyles();
+  return (
+    <Box>
+      <Box>
+        <svg width={width || "400px"} height={height || "400px"}>
+            <circle cx="200" cy="100" r="90" className={classes.circle1}/>
+            <circle cx="140" cy="200" r="90" className={classes.circle2}/>
+            <circle cx="260" cy="200" r="90" className={classes.circle3}/>
+          </svg>
+      </Box>
+    </Box>
+  );
+};
+
+export default Visage;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { render } from "react-dom"
-import Svg from './Visage'
+import Svg from './Op'
 
 const App = () => {
   return (
